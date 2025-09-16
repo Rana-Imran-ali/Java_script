0f9753c202f8922123093f7fff59474d728465d5
Imran Ali <ranaimranali2210@gmail.com>
2025-09-16 03:48:05 -0700

    Template literal

--- App.js
+++ App.js
@@ -4,3 +4,28 @@ console.log("Java script");
 let a=10;
 let b=5;
 console.log("Sum is:",a + b);
+let pencilprice=10;
+let erasorprice=12;
+//console.log("Total price is :",pencilprice + erasorprice, "rupee");
+let output=`The second total price: ${pencilprice} Rupee.`;  // These are template litreal
+console.log(output);
+// comparison opertors 
+let age=18;
+console.log(age>18);
+console.log(age<=18);
+// condition statement
+console.log("Bofore my if statement:");
+let Age=23;
+if (Age>=18){
+    console.log("You can vote");
+}
+
+console.log("After my statement");
+if (Age>20){
+    console.log("You are in ypurs 20s");
+}
+
+// Alert and prompt
+alert("This is Java script file");
+// prompt
+prompt("Enter your name");

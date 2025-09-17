aa055deb2353eb3325afc3c37d35447c741990ce
Imran Ali <ranaimranali2210@gmail.com>
2025-09-17 03:49:41 -0700

    object litrels

---
+++ Task 3/litreals.js
@@ -0,0 +1,17 @@
+
+// method of writing litrel
+let student={
+  name: "Imran",  // keys  
+  age : 23,
+  marks : 80,
+
+
+};
+const post={
+    username: "ranaimranali2210@gmail.com",
+    content: "This is my first post",
+    likes: 150,
+    repost: 5,
+    tags:["@apna collete","Imran"]
+
+};

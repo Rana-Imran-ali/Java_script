2529145fdbb62f191424b822d73fc55974215b2a
Imran Ali <ranaimranali2210@gmail.com>
2025-09-18 06:57:11 -0700

    Functions

--- Task 4/app.js
+++ Task 4/app.js
@@ -8,3 +8,22 @@ function printInfo(name,age){
 console.log(`${name} ${age}.`);
 }
 printInfo("Ali",20);
+// sum function
+function sum(a,b){
+    console.log(a+b);
+}
+sum(1,2);
+// average of three number
+function avg(a,b,c){
+    let avg=(a+b+c)/3
+    console.log (avg)
+}
+avg(2,5.9);
+
+// multiplicaton table number
+function printTable(n){
+    for(let i=n;i<=n*10;i+=10){
+        console.log(i);
+    }
+}
+printTable(3);

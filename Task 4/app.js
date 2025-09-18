9afb7e7ff079edf21ad8f9f8a8108c491b47745e
Imran Ali <ranaimranali2210@gmail.com>
2025-09-18 04:32:26 -0700

    Functions

---
+++ Task 4/app.js
@@ -0,0 +1,10 @@
+// Function arrguments
+function printName(name){
+    console.log(name);
+}
+printName("Imran");
+
+function printInfo(name,age){
+console.log(`${name} ${age}.`);
+}
+printInfo("Ali",20);
